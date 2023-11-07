621e671c96eb40ddd16dc82e8a4f3831d2be9f3d
sanat <sanat918@gmail.com>
2023-11-07 23:35:41 +0530

    Animation effect Added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className='fade-in-image'>
         <img src={logo} className="App-logo" alt="logo" />
+        </div>
       
       </header>
     </div>
